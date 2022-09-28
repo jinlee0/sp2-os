8ed2e3540cf26b94efaff3039985d2acc34133d6
jinlee0 <wls4370@gmail.com>
2022-09-28 14:54:39 +0900

    0928 수업

--- src/main/java/os/Loader.java
+++ src/main/java/os/Loader.java
@@ -1,7 +1,11 @@
 package main.java.os;
 
 public class Loader {
-    public Process load(String processName) {
+    public Process load(String exeName) {
+        // read file
+        // parse header
+        Process process = new Process();
+//        process.initialize(exeName);
         return new Process();
     }
 }
