84cbd2b9bd2664a9dddb8bde68e16cbc3498e39b
jinlee0 <wls4370@gmail.com>
2022-10-19 14:59:17 +0900

    1019 수업

--- src/main/java/os/Scheduler.java
+++ src/main/java/os/Scheduler.java
@@ -11,15 +11,25 @@ import java.io.FileNotFoundException;
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Semaphore;
 
 public class Scheduler extends Thread{
     private final CPU cpu = CPU.getInstance();
     private final Loader loader = new Loader();
 
-    private final ProcessQueue readyQueue = new ProcessQueue();
     private final ProcessQueue waitQueue = new ProcessQueue();
     private Process runningProcess = new Process();
     private InterruptHandler interruptHandler = new InterruptHandler(this);
+
+    public Scheduler() {
+        readyQueue = new ProcessQueue();
+        readyQueueEmptySemaphore = new Semaphore(READY_QUEUE_MAX_SIZE, true);
+        readyQueueFullSemaphore = new Semaphore(READY_QUEUE_MAX_SIZE, true);
+        readyQueueEmptySemaphore.drainPermits();
+        readyQueueSemaphore = new Semaphore(1, true);
+    }
 
     public void init() {
         try {
@@ -49,18 +59,53 @@ public class Scheduler extends Thread{
         cpu.addInterrupt(new ProcessInterrupt(EProcessInterrupt.PROCESS_START, process));
     }
 
+    // critical section
+    private static final int READY_QUEUE_MAX_SIZE = 10;
+    private final ProcessQueue readyQueue;
+    private final Semaphore readyQueueEmptySemaphore; // init all acquired
+    private final Semaphore readyQueueFullSemaphore;
+    private final Semaphore readyQueueSemaphore;
+    BlockingQueue<Process> queuee = new LinkedBlockingQueue<>(READY_QUEUE_MAX_SIZE);
     public synchronized void enReadyQueue(Process process) {
-        this.readyQueue.enqueue(process);
+        try {
+            readyQueueSemaphore.acquire();
+            readyQueueFullSemaphore.acquire();
+            this.readyQueue.enqueue(process);
+            readyQueueEmptySemaphore.release();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            readyQueueSemaphore.release();
+        }
     }
     public synchronized Process deReadyQueue() {
-        return this.readyQueue.dequeue();
+        try {
+            readyQueueSemaphore.acquire();
+            readyQueueEmptySemaphore.acquire();
+            Process process = this.readyQueue.dequeue();
+            readyQueueFullSemaphore.release();
+            return process;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            readyQueueSemaphore.release();
+        }
+        return null;
     }
     public synchronized boolean isReadyQueueEmpty() {
         return readyQueue.isEmpty();
     }
     public synchronized void removeFromReadyQueue(Process interruptedProcess) {
-        readyQueue.remove(interruptedProcess);
+        try {
+            readyQueueSemaphore.acquire();
+            readyQueue.remove(interruptedProcess);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            readyQueueSemaphore.release();
+        }
     }
+    /////////////////////
 
     public Process getRunningProcess() {
         return runningProcess;
@@ -87,8 +132,8 @@ public class Scheduler extends Thread{
             return queue.isEmpty();
         }
 
-        private void remove(Process interruptedProcess) {
-            queue.remove(interruptedProcess);
+        private void remove(Process process) {
+            queue.remove(process);
         }
     }
 
